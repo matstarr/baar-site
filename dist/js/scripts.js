0c7649e94a35cf5bce5241e0cadfa03ccf176b50
mat.1985 <sm0k3@gmx.de>
2017-04-09 23:11:27 +0200

    big menu

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -9,7 +9,21 @@ console.log('plugins');
 console.log('main');
 
 $(document).ready(function(){
-  $('.navbar').hide(10).delay(800).fadeIn(800);
+    $('.navbar').hide(10).delay(800).fadeIn(800);
+    $('.header-text').hide(10).delay(800).fadeIn(800);
 });
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 100) {
+        $(".navbar-default").addClass('scrolled');
+        $(".navbar-default").removeClass('navbar-static-top').addClass('navbar-fixed-top');
+    } else {
+        $(".navbar-default").removeClass('scrolled');
+        $(".navbar-default").removeClass('navbar-fixed-top').addClass('navbar-static-top');
+    }
+});
+
 
 //# sourceMappingURL=scripts.js.map
